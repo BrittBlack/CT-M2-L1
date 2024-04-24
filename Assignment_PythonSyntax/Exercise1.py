971985af6bd31f39e2133d5a2dd8313d6a3ee1f7
BrittBlack <97366121+BrittBlack@users.noreply.github.com>
2024-04-23 21:54:19 -0500

    Exercise 1

---
+++ Assignment_PythonSyntax/Exercise1.py
@@ -0,0 +1,26 @@
+#Showcase Your Dance Moves!
+#And I will with Task 1 and Task 2
+#
+#
+#
+#
+#Task 1: Code Correction
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#
+#
+#
+#
+#Task 2: Your Mood Today
+
+user_response = input ("How is your mood today?")
+
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
